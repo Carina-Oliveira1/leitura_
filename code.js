1562dbc4ea2a582ab2d1111d55a1639b4cff79eb
Carina-Oliveira1 <carina65483@estudante.ifb.edu.br>
2025-05-01 18:25:55 -0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,38 @@
+function nomec()
+		{
+			var valor = document.getElementById('nome');
+	
+			if(valor.value.length < 5)
+			{
+				alert("O nome deve ter pelo menos 5 caracteres.");
+			}
+		}
+
+		function emailc()
+		{
+			var valor2 = document.getElementById('email1');
+	
+			if(valor2.value.length < 5)
+			{
+				alert("O e-mail deve ter pelo menos 5 caracteres.");
+			}
+		}
+
+		function msgc()
+		{
+			var valor3 = document.getElementById('msg');
+	
+			if(valor3.value.length < 20)
+			{
+				alert("O mínimo de caracteres neste campo é 20.");
+			}
+			else if(valor3.value.length > 200)
+			{
+				alert("O máximo de caracteres neste campo é 200.");
+			}
+		}
+
+		function enviada()
+		{
+			alert("Mensagem eviada com sucesso!");
+		}
